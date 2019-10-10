367fcdac96f318887f51fe81334b704ea6a61c95
Ethan Hall <eah1759@g.coppellisd.com>
2019-10-10 17:14:27 -0500

    System Rework
    Reworked the js system for compatibility with multiple dice

--- scripts.js
+++ scripts.js
@@ -1,47 +1,60 @@
-var d = 1;
-var f = 20;
-var m = 0;
+var h = 1;//addMore
 
-function addDice(x)
+function addDice(x,a)
 {
+   var d = parseInt(document.getElementById("xd"+a).innerHTML, 10);
    switch(x)
    {
       case 1: d++;break;
       case -1: if(d>1){d--;}break;
       default: alert("...how?");
    }
-   document.getElementById("xd").innerHTML = d;
+   document.getElementById("xd"+a).innerHTML = d;
 }
 
-function addFace(x)
+function addFace(x,a)
 {
+   var f = parseInt(document.getElementById("dx"+a).innerHTML, 10);
    switch(x)
    {
       case 1: f++;break;
       case -1: if(f>1){f--;}break;
       default: alert("...how?");
    }
-   document.getElementById("dx").innerHTML = f;
+   document.getElementById("dx"+a).innerHTML = f;
 }
 
-function addMod(x)
+function addMod(x,a)
 {
+   var m = parseInt(document.getElementById("c"+a).innerHTML, 10);
    switch(x)
    {
       case 1: m++;break;
       case -1: m--;break;
       default: alert("...how?");
    }
-   document.getElementById("c").innerHTML = m;
+   document.getElementById("c"+a).innerHTML = m;
 }
 
 function doThing()
 {
    var t = 0;
-   for(var i = 0; i < d; i++)
+   for(var a = 1; a <= h; a++)
    {
-      t = t + Math.trunc((Math.random()*f)+1);
+      var d = parseInt(document.getElementById("xd"+a).innerHTML, 10);
+      var f = parseInt(document.getElementById("dx"+a).innerHTML, 10);
+      var m = parseInt(document.getElementById("c"+a).innerHTML, 10);
+      for(var i = 0; i < d; i++)
+      {
+         t = t + Math.trunc((Math.random()*f)+1);
+      }
+      t = t + m;
    }
-   t = t + m;
    document.getElementById("result").innerHTML = t;
 }
+
+/*function addMore()
+{
+   h++;
+   document.getElementById("addStuff").innerHTML = document.getElementById("addStuff").innerHTML + "<br><h3><a id='xd"+h+"'>1</a>d<a id='dx"+h+"'>20</a> +<a id='c"+h+"'>0</a></h3><h4><a onclick='addDice(1,"+h+");'>+1</a>  <a onclick='addDice(-1,"+h+");'>-1</a> | <a onclick='addFace(1,"+h+");'>+1</a>  <a onclick='addFace(-1,"+h+");'>-1</a> | <a onclick='addMod(1,"+h+");'>+1</a>  <a onclick='addMod(-1,"+h+");'>-1</a></h4><br>"
+}*/
